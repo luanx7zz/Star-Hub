2992fbe5f618abca41da31249d7f7849002dec2a
luanx7zz <luanpereira04082011@gmail.com>
2026-02-15 13:36:21 -0300

    Create Permission.lua

---
+++ Permission.lua
@@ -0,0 +1,21 @@
+----------///// SISTEMA-ADMIN /////------------
+    allowedUsers = {
+    ----------///// TIK-TOK /////------------
+    
+    
+    
+    ----------///// TEMPORARIOS /////-------------
+   
+   
+    
+    ----------///// PERMANENTE ACESS /////------------
+    ["luan04082011"] = true,
+    ["ITZ_LuanJefferson"] = true,
+    ["jog7protrem"] = true,
+   
+    ------STAFF
+    ["anthony22lucca"] = true,
+    
+    }
+    
+    return allowedUsers
